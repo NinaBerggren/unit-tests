7f9a8d39ca29a2c3980fe3147b732d8b986398d8
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-11-20 22:10:32 +0100

    done test to 7

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,28 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
+  // if (items.length === 2) return `First: ${items[0]}, Last: ${items[1]}`;
+  //   if (items.length === 5)
+  //     return `First: ${items[0]}, Last: ${items[items.length - 1]}`;
+  //   if (items.length === 1) return `Only item: ${items}`;
+  //   if (items.length === 0) return `No items!`;
+
+    if (items.length === 2) {
+      return `First: ${items[0]}, Last: ${items[1]}`
+    }
+    else if (items.length > 2){
+      return `First: ${items[0]}, Last: ${items[items.length-1]}`
+    }
+    else if (items.length === 1){
+      return `Only item: ${items[0]}`
+    }
+    else if (items.length === 0){
+      return `No items!`
+    }
+
+// if (!items.length) {
+//   return `First: ${items[0]}, Last: ${items[items.length -1 ]}`
+// } else if (items.length === 1) {
+//   return `Only item: ${items[0]}`
+// } else {
+//   return `No items!`
+// } 
 }
--- src/5-month-name.js
+++ src/5-month-name.js
@@ -3,6 +3,12 @@ const months = [
   'July', 'August', 'September', 'October', 'November', 'December'
 ]
 
-export const monthName = (monthNumber) => {
-  return months[monthNumber]
+export const monthName = (monthNumber) => {  
+  if (monthNumber < 1) {
+    return null
+  } else if (monthNumber > 12) {
+    return null
+  } else {  
+    return months[monthNumber - 1]
+  }
 }
